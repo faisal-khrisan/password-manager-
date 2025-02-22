733f35b7c1e15e77b805980d39e33230dcad4213
Faisal Khrisan <faisalkhrisan@gmail.com>
2025-02-22 21:57:47 +0800

    create a file for exception handling

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print ("hello word")
+try :
+    file = open ('file.txt')
+    content = file.read()
+except FileNotFoundError :
+    file = open ("fil.txt", "w")
+    file.write("after exception")
+else:
+    content = file.read()
+    print(content)
