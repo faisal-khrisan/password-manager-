bfc696e1ab061875302d21e995d3a19ee0776f9e
Faisal Khrisan <faisalkhrisan@gmail.com>
2025-02-22 22:02:58 +0800

    adjusting in the code for the creating file and the exception

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 try :
     file = open ('file.txt')
-    content = file.read()
+
 except FileNotFoundError :
-    file = open ("fil.txt", "w")
+    file = open ("file.txt", "w")
     file.write("after exception")
 else:
     content = file.read()
     print(content)
+finally:
+    file.close()
+    print ("File was closed.")
