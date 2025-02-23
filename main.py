7cc85828864976ec10af96a37a87743ac1cc6ba1
Faisal Khrisan <faisalkhrisan@gmail.com>
2025-02-23 23:32:45 +0800

    Program is ready, Although there an json exception error still exist if the file is empty and exsit and the other ways does not work

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+import json
+from json import JSONDecodeError
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+# ---------------------------- SEARCH FOR DATA------------------------------- #
+def search():
+    # using website name for searching
+
+    search_by_website = website_entry.get()
+
+    # Load data from the Json file and also handle exception
+
+    try:
+        with open("data.json", "r") as search_for_data:
+            data_to_be_found = json.load(search_for_data)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+
+    # check the data that needed to be found in the dictionary
+    if search_by_website in  data_to_be_found:
+        messagebox.askokcancel(title=search_by_website,message=f"Website data : "
+        f"\nEmail: {data_to_be_found[search_by_website]["email"]}"
+        f"\nPassword: {data_to_be_found[search_by_website]["password"]} \n")
+    else:
+        messagebox.showinfo(title="Error", message=f"No details about {search_by_website} ")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
 def generate_password():
+    password_entry.delete(0, END)
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -28,17 +53,58 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        },
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                       f"\nPassword: {password} \nIs it ok to save?")
+
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
+            # Promt the user if the data saved or not
+                try:
+
+                    with open("data.json", "r") as search_for_data:
+                        data_to_be_found = json.load(search_for_data)
+
+
+                        if website in data_to_be_found:
+                            messagebox.showinfo(title="Successful", message="Data saved Successfully")
+                        else:
+                            messagebox.showinfo(title="Opps", message="Opps unable to save data !")
+
+                except KeyError:
+                    messagebox.showinfo(title="Error", message="Unable to open the file!")
+
+
+
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -61,16 +127,18 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=45)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=45)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
+email_entry.insert(0, "example@gmail.com")
+password_entry = Entry(width=28)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button (text= "Search", width=10, command= search)
+search_button.grid(row=1,column=3)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
